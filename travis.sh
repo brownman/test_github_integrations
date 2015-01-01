23b3e10ab0b307299831c822fd6a93d9e93f08a1
brownman <brownman556@gmail.com>
2015-01-01 17:50:38 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -2,4 +2,4 @@ git clone --depth=1 https://github.com/brownman/github_integrations.git
 cd github_integrations
 source config.cfg
 chmod +x *.sh . -R
-bash -c BANK/travis_and_github/get_private_key.sh
+bash -x BANK/travis_and_github/get_private_key.sh
