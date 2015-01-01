43ff9a4068d3500aba9d708f668e668c9f4eebc0
brownman <brownman556@gmail.com>
2015-01-01 18:38:05 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 set -u
 
-export dir_self=$( cd `dirname $0`; echo $PWD; )
+export dir_base=$( cd `dirname $0`; echo $PWD; )
 export repo1='github_integrations'
-echo $dir_self
-test -d $dir_self
+export dir_self=$dir_base/$repo1
 
 
 git clone --depth=1 https://github.com/brownman/${repo1}.git $repo1
-cd $repo1
-chmod +x ./travis.sh
-./travis.sh
-source $dir_self/config.cfg
-
-$dir_self/ensure.sh && { bash -c $dir_self/BANK/travis_and_github/get_private_key.sh; } || {
-  echo 'YOU NEED TO UPDATE .travis.yml;'
-  echo 'HELP YOURSELF BY VISITING:'
-  echo 'https://github.com/brownman/github_integrations';
-}
+chmod +x $dir_self/travis.sh
+bash -c $dir_self/travis.sh
