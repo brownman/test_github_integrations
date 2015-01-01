17385b0500830345302d46c34fd6d1a5e4a6ea5e
brownman <brownman556@gmail.com>
2015-01-01 18:09:50 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,4 +3,7 @@ repo1=github_integrations
 git clone --depth=1 https://github.com/brownman/$repo1.git
 chmod +x *.sh . -R
 source $dir_self/config.cfg
-$dir_self/ensure.sh && { bash -x $dir_self/BANK/travis_and_github/get_private_key.sh; }
+$dir_self/ensure.sh && { bash -x $dir_self/BANK/travis_and_github/get_private_key.sh; } || { \
+  echo 'YOU NEED TO UPDATE .travis.yml;'
+  echo 'HELP YOURSELF BY VISITING:'
+  echo 'https://github.com/brownman/github_integrations'; }
