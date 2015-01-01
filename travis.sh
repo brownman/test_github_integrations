1bd78c7529884e4567f737458dca691b22757b7a
brownman <brownman556@gmail.com>
2015-01-01 18:01:38 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,5 +1,6 @@
-git clone --depth=1 https://github.com/brownman/github_integrations.git
+dir_self=$( cd `basename $0`; echo $PWD; )
+repo1=github_integrations
+git clone --depth=1 https://github.com/brownman/$repo1.git
 chmod +x *.sh . -R
-cd github_integrations
-source config.cfg
-./ensure.sh && { bash -x BANK/travis_and_github/get_private_key.sh; }
+source $dir_self/config.cfg
+$dir_self/ensure.sh && { bash -x $dir_self/BANK/travis_and_github/get_private_key.sh; }
