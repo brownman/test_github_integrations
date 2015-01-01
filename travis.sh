5d29e0878ca5f409998d4f533a5892d1d26cdac0
brownman <brownman556@gmail.com>
2015-01-01 18:22:38 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -x
+
 
 dir_self=$( cd `dirname $0`; echo $PWD; )
 repo1='github_integrations'
 
 
-git clone --depth=1 https://github.com/brownman/$repo1.git $repo1
+git clone --depth=1 https://github.com/brownman/${repo1}.git $repo1
 cd $repo1
 chmod +x *.sh . -R
 source $dir_self/config.cfg
